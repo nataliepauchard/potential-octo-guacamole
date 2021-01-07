1c5bf09af04887c89dedb3bd196b09b2b7e4e96d
Natalie Pauchard <natalie.pauchard@gmail.com>
2021-01-08 03:35:10 +1100

    Tic_Tac_Toe_Project_Zero

---
+++ README.md
@@ -0,0 +1,53 @@
+
+# Project 0: Tic-Tac-Toe
+
+### The GAME
+
+Tic-Tac-Toe is a game that's also called naughts and crosses. It's a simple 2 player game where each player selects a square within a 3x3 grid and the aim is it get three squares in a row. This was a simple project for training purposes and still a work in progress.
+
+It was meant to help us learn how html, CSS, Javascript and jQuery all interact to create a game we can deploy online.
+
+### Approach
+
+At first the approach considered two functions (as per below), that referred to two Player buttons. However this caused unnecessary problems down the line when adding different class was required.
+
+```
+$('#PlayerX').click(function() {
+  $('#screen').text("Once Player X has made their selection. Click Player O");
+  box.click(function() {
+  $(this).css('background', 'red').html('X');
+  });
+winCheck("X")
+});
+
+$('#PlayerO').click(function() {
+  $('#screen').text("Once Player O has made their selection. Click Player X");
+  $('.square').click(function() {
+  $(this).css('background', 'blue').html('O');
+  });
+  winCheck("O")
+});
+```
+Therefore, one function was eventually created to discern between first player and second player (later termed 'Felix' and 'Salem').
+
+### Tic-Cat-Toe
+
+The game's name was slightly altered to reflect the theme of the page and also a play on words, especially the term 'Cat's Game' which means when two players draw. Instead of naughts and crosses the game uses images and names of cats.
+
+### Bugs
+
+- The project is still a work in progress, especially the selection of players at the start with many potential user errors.
+
+- While the game mostly works as it should, occasionally some combinations don't display exactly what coincides with the game and a 'reset' button is required.
+
+- It anticipates a draw before you get to the very end. Even though there's no way of either player winning it displays Cat's Draw msging prematurely.
+
+### Next Steps
+
+- A player selection method that is less error prone.
+
+- More attractive animations.
+
+- De-bugging certain combinations.
+
+- Show the cat names on each square as they're selected
---
+++ js/main.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+
+
+let player1;
+let player2;
+let player1Turn = true;
+let turnCounter = 0;
+
+
+$('.square').click(function() {
+
+  if (player1Turn && $(this).attr('class') === 'square') {
+    player1Turn = false;
+    $(this).addClass(player1).html();
+    turnCounter += 1;
+    winCheck(player1, turnCounter);
+  }
+  else if (!player1Turn && $(this).attr('class') === 'square') {
+    player1Turn = true;
+    $(this).addClass(player2).html();
+    turnCounter += 1;
+    winCheck(player2, turnCounter);
+  }
+});
+const playerChoice = function () {
+  player1 = prompt("Are you a 'Felix' or a 'Salem'? (Type cat name below)"); //was asking whether player was "X" or "O"
+  if (player1 === "Felix") { //was "X"
+    player2 = "Salem" // was "O"
+  } else {
+   player2 = "Felix" //was "X"
+  }
+}
+
+$('#play').click(playerChoice);
+
+$('#play').click(function() {
+  $(this).addClass('clickPlay').html('GOOD LUCK!');
+});
+
+
+
+// //Display 'win' 'lose' or 'draw'
+// // const possibleWins = [
+// //     [0, 1, 2],
+// //     [3, 4, 5],
+// //     [6, 7, 8],
+// //     [0, 3, 6],
+// //     [1, 4, 7],
+// //     [2, 5, 8],
+// //     [0, 4, 8],
+// //     [2, 4, 6]
+// // ];
+
+const winCheck = function(player, turnCounter){
+  const sqr0 = $('#sqr0').attr('class');
+  const sqr1 = $('#sqr1').attr('class');
+  const sqr2 = $('#sqr2').attr('class');
+  const sqr3 = $('#sqr3').attr('class');
+  const sqr4 = $('#sqr4').attr('class');
+  const sqr5 = $('#sqr5').attr('class');
+  const sqr6 = $('#sqr6').attr('class');
+  const sqr7 = $('#sqr7').attr('class');
+  const sqr8 = $('#sqr8').attr('class');
+
+    if (sqr0 === sqr1 && sqr1 === sqr2  && sqr0 !== 'square') {
+        const outcome = (`Nice one ${player}, YOU WON!`);
+        $('#screen').html(outcome);
+      }
+      else if (sqr3 === sqr4 && sqr4 === sqr5  && sqr3 !== 'square') {
+        const outcome = (`Nice one ${player}, YOU WON!`);
+        $('#screen').html(outcome);
+      }
+      else if (sqr6 === sqr7 && sqr7 === sqr8  && sqr6 !== 'square') {
+        const outcome = (`Nice one ${player}, YOU WON!`);
+        $('#screen').html(outcome)
+      }
+      else if (sqr0 === sqr3 && sqr3 === sqr6  && sqr0 !== 'square') {
+        const outcome = (`Nice one ${player}, YOU WON!`);
+        $('#screen').html(outcome);
+      }
+      else if (sqr1 === sqr4 && sqr4 === sqr7  && sqr1 !== 'square') {
+        const outcome = (`Nice one ${player}, YOU WON!`);
+        $('#screen').html(outcome);
+    }
+    else if (sqr2 === sqr5 && sqr5 === sqr8  && sqr2 !== 'square') {
+      const outcome = (`Nice one ${player}, YOU WON!`);
+      $('#screen').html(outcome);
+  }
+  else if (sqr0 === sqr4 && sqr4 === sqr8  && sqr0 !== 'square') {
+    const outcome = (`Nice one ${player}, YOU WON!`);
+    $('#screen').html(outcome);
+}
+else if (sqr2 === sqr4 && sqr4 === sqr6  && sqr2 !== 'square') {
+  const outcome = (`Nice one ${player}, YOU WON!`);
+  $('#screen').html(outcome);
+}
+if (turnCounter == 9) {
+  const outcome = ("IT'S A CAT'S GAME! Refresh the page & play again.");
+  $('#screen').html(outcome);
+}
+}
+
+
+}); //document.ready don't delete!!!
+
+// Reset button required
